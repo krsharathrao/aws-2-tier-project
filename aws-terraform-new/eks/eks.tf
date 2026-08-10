dea4806e32e12f3e118f062b4d7e1404d1077b37
krsharathrao <krsharathrao@gmail.com>
2026-08-10 19:40:19 +0530

    Update eks.tf

--- aws-terraform-new/eks/eks.tf
+++ aws-terraform-new/eks/eks.tf
@@ -97,7 +97,7 @@ resource "aws_eks_node_group" "eks_nodegroup" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name  = aws_eks_cluster.eks.name
   addon_name    = "aws-ebs-csi-driver"
-  addon_version = var.ebs_csi_driver_version
+  #addon_version = var.ebs_csi_driver_version
 
   depends_on = [aws_eks_node_group.eks_nodegroup]
 }
